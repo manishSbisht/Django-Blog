7e88218e34c766988a7e1af328149f4d12531112
manishSbisht <manibhi98@gmail.com>
2019-04-23 02:19:13 +0530

    Added dummy post data

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,28 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# dummy data
+post_list = [
+    {
+        'author': 'Manish',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'August 11, 1998'
+    },
+    {
+        'author': 'Sankalp',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'August 12, 1998'
+    },
+    {
+        'author': 'Ritik',
+        'title': 'Blog Post 3',
+        'content': 'Third Post Content',
+        'date_posted': 'August 13, 1998'
+    }
+]
+
 
 def home(request):
     return render(request, 'blog/home.html')
