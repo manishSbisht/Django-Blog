d3a76eefb205c927880f1c1eb28512ab2f14bfcb
manishSbisht <manibhi98@gmail.com>
2019-06-08 17:03:41 +0530

    Create custom form class to replace 'UserCreationForm'

---
+++ users/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.fofrms import UserCreationForm
+
+# Creating a custom form class by inheriting from 'UserCreationForm'
+
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()  # add the new custom field
+
+    class Meta:  # meta class tells what do with parent form class
+        model = User  # save data of this class to model 'User'
+
+        fields = ['username', 'email', 'password1', 'password2']
+        # form will have these fields in this order
