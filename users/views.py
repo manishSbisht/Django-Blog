78cafb839c45c0e1d659b94522e388bc47e73f48
manishSbisht <manibhi98@gmail.com>
2019-06-08 12:41:55 +0530

    hold submitted data from post request

--- users/views.py
+++ users/views.py
@@ -4,5 +4,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 def register(request):
-    form = UserCreationForm
+    if request.method == 'POST':  # if form is submitted
+        form = UserCreationForm(request.POST)
+        # hold submitted data in 'form'
+    else:
+        # else show a new empty form
+        form = UserCreationForm
+
     return render(request, 'users/register.html', {'form': form})
