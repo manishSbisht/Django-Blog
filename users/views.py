ef48707c3c7d467cdefe23f3b22b871de9b2bdf5
manishSbisht <manibhi98@gmail.com>
2019-06-08 13:17:39 +0530

    Add message for successful account creation

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
 
 
@@ -7,8 +8,13 @@ def register(request):
     if request.method == 'POST':  # if form is submitted
         form = UserCreationForm(request.POST)
         # hold submitted data in 'form'
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')  # cleaned_data is data after validation
+            messages.success(request, f'Account created for {username}!')
+
     else:
         # else show a new empty form
-        form = UserCreationForm
+        form = UserCreationForm()
 
     return render(request, 'users/register.html', {'form': form})
