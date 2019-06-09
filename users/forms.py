ccc32b2c17624d1247908b4796a7ce62bc484906
manishSbisht <manibhi98@gmail.com>
2019-06-09 22:09:31 +0530

    Create forms for User update and Profile update

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
 # Creating a custom form class by inheriting from 'UserCreationForm'
 
 
@@ -13,3 +13,22 @@ class UserRegistrationForm(UserCreationForm):
 
         fields = ['username', 'email', 'password1', 'password2']
         # form will have these fields in this order
+
+
+class UserUpdateForm(forms.ModelForm):
+    # ModelForm allow to create form for a specific database model
+    email = forms.EmailField()
+
+    class Meta:
+        model = User  # save data of this class to model 'User'
+
+        fields = ['username', 'email']
+        # form will have these fields in this order
+
+
+class ProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile  # save data of this class to model 'User'
+
+        fields = ['image']
