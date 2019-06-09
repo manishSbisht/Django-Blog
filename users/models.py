e1e53ebb69d1428efc2f5156065b0bf910301d22
manishSbisht <manibhi98@gmail.com>
2019-06-09 10:29:08 +0530

    Create Profile model with image field and link with User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=CASCADE)
+    # user profile have one to one relationship with User model
+    # on delete of User object, related profile object will also be deleted,
+    # but not the other way around.
+
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    # pictures wll be stored in the profile_pics/ dir.
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
