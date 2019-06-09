54b2cdbbac18c8666a455a05bd0ece03acbf6c65
manishSbisht <manibhi98@gmail.com>
2019-06-09 11:34:54 +0530

    add Mediaroot and mediaurl to urlpatters

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,7 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('profile/', user_views.profile, name="profile"),
 ]
+
+# if in Devlpment env, add media url and media root path to url patterns
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
