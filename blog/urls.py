01f15be2b7eddfd995406875b8ba5a02538c8e61
manishSbisht <manibhi98@gmail.com>
2019-06-23 02:10:19 +0530

    Add route for individual post with its ID in URL

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from . import views
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('home/', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
 ]
 
 # as_view() method converts class to a view
+
+# <int:pk> is the variable part of URL,
+# pk=primary key, int=expects the value to be integer
+# name 'pk' is predefined in DetailView class
