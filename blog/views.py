384f0e7d54c64f4d5d6c50d4f285aed7645d4475
manishSbisht <manibhi98@gmail.com>
2019-07-09 12:12:34 +0530

    Fixes #9: Post can only be updated by their authors

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 def home(request):
@@ -40,7 +40,9 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    # all the inherited mixins should be passed to the left of 'UpdateView'
+
     model = Post
     fields = ['title', 'content']
     # defualt template name is: <model>_form.html
@@ -52,6 +54,15 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
 
         # now run the modified form_valid() method after above change
         return super().form_valid(form)
+
+    # test the current object against a condition
+    def test_func(self):
+        post = self.get_object()  # return the object view is displaying
+
+        # if current user is author of current post
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
